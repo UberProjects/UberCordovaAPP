0cc2caf3a859ca35020ec4cb27777228ff51f4dd
Irakli Zhuzhunashvili <irakli.zhuzhunashvili1@gmail.com>
2016-04-11 14:03:33 -0600

    Added pending ride

--- www/modules/uber_core/controllers/pending_ride.controller.client.js
+++ www/modules/uber_core/controllers/pending_ride.controller.client.js
@@ -6,7 +6,33 @@
 angular.module('uber_core').controller('PendingRideController', [
     '$scope',
     '$cordovaGeolocation',
-    function ($scope, $cordovaGeolocation) {
+    'Ride',
+    'RideRoutes',
+    function ($scope, $cordovaGeolocation, Ride, RideRoutes) {
 
+        $scope.data = {};
+
+        $scope.getEstimates = function() {
+            var start_pos = {
+                'lat': 39.7392360,
+                'long': -104.9902510
+            };
+
+            var end_pos = {
+                'lat': 40.0149860,
+                'long': -105.2705460
+            };
+
+            RideRoutes.getEstimatedPrice(start_pos, end_pos).then(function (res) {
+                console.log(res.data.message);
+                $scope.data.rideEstimates = res.data.message.prices;
+            });
+        };
+
+        $scope.startRide = function(product_id) {
+            console.log(product_id);
+        };
+
+        $scope.getEstimates();
     }
 ]);
